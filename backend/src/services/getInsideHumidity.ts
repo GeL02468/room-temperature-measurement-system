2e6d3121e8dd01ea63d009f9a0263f8f7f7e21be
GeL <gel02468@gmail.com>
2026-02-08 01:34:51 +0900

    fix: 不要部分の削除

--- backend/src/services/getInsideHumidity.ts
+++ backend/src/services/getInsideHumidity.ts
@@ -2,9 +2,7 @@
 import { prisma } from '../lib/prisma'
 
 export const getInsideHumidity = async () => {
-  const measurementArray = await prisma.inside_measure.findMany({
-    where: { inside_humidity: {} },
-  })
+  const measurementArray = await prisma.inside_measure.findMany()
   let resultArray: number[] = []; // 該当データ抽出用変数
   for (const measure of measurementArray) {
     resultArray.push(measure.inside_humidity);
