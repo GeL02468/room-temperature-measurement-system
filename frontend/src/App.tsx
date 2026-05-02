255a08e917288ca12f9a832e2e14c9b95a47bf61
GeL02468 <gelworks02468@gmail.com>
2026-05-02 16:12:27 +0000

    feat: フロントエンド、ホーム画面の作成（たたき台）

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import './App.css'
 import * as word from '../word.json'
+import InsideInfo from './InsideInfo.tsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{ word.title }</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        <span></span>
+        {word.title}
+      </h1>
+      <BrowserRouter>
+        <div className="header-items">
+          <nav>
+            <Link to="/Inside" className="header-item">気温情報</Link>
+            <Link to="/Inside" className="header-item">湿度情報</Link>
+            <Link to="/Inside" className="header-item">気圧情報</Link>
+            <Link to="/Inside" className="header-item">設定</Link>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/Inside" element={<InsideInfo />} />
+        </Routes>  
+      </BrowserRouter>
     </>
   )
 }
-
 export default App
---
+++ frontend/src/InsideInfo.tsx
@@ -0,0 +1,10 @@
+import './InsideInfo.css'
+
+function Home() {
+  return (
+    <>
+      <p>室内測定情報</p>
+    </>
+  )
+}
+export default Home
